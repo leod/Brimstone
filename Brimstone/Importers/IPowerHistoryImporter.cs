e7b5fbcbf3aeaef382edae479927dbafa8786692
Leonard Dahlmann <leo.dahlmann@gmail.com>
2016-11-14 20:54:53 +0100

    Add extension methods to allow importing power histories by giving a filename

--- Brimstone/Importers/IPowerHistoryImporter.cs
+++ Brimstone/Importers/IPowerHistoryImporter.cs
@@ -8,4 +8,19 @@ namespace Brimstone.Importers
 		PowerHistory ImportFirst(StreamReader stream);
 		IEnumerable<PowerHistory> Import(StreamReader stream);
 	}
+
+	public static class PowerHistoryImporterExtensions
+	{
+		public static PowerHistory ImportFirst(this IPowerHistoryImporter importer, string filename) {
+			using (StreamReader stream = new StreamReader(filename)) {
+				return importer.ImportFirst(stream);
+			}
+		}
+
+		public static IEnumerable<PowerHistory> Import(this IPowerHistoryImporter importer, string filename) {
+			using (StreamReader stream = new StreamReader(filename)) {
+				return importer.Import(stream);
+			}
+		}
+	}
 }
